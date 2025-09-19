1dfb17e223efe0d0989148dacf160876d9bfc975
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-09-19 18:04:13 +0300

    integrate @nuxtjs/strapi module and update middleware configurations

--- apps/backend/config/middlewares.ts
+++ apps/backend/config/middlewares.ts
@@ -1,8 +1,24 @@
 export default [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'frame-ancestors': ["'self'", 'http://localhost:3000'],
+        },
+      }
+    }
+  },
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
