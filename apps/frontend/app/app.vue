e5796134d8ab119451f0c80a8efc4e9101baa8fd
Elijah Ellanski <ilya.ellanskiy@itms.tech>
2025-12-25 16:35:36 +0300

    scaffold

--- apps/frontend/app/app.vue
+++ apps/frontend/app/app.vue
@@ -1,6 +1,35 @@
+<script setup lang="ts">
+import type { ApiPostPostDocument } from '@drovovoz/api-client'
+
+const { find } = useStrapi()
+const { data: posts } = await find<ApiPostPostDocument>('posts')
+
+const { locales, setLocale } = useI18n()
+
+const switchLocalePath = useSwitchLocalePath()
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>{{ $t('welcome') }}</h1>
+    <h2>{{ $t('posts') }}</h2>
+    <pre>{{ posts }}</pre>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.excerpt }}</p>
+        <NuxtTime :datetime="post.publishedAt" :locale="$i18n.locale" />
+      </li>
+    </ul>
+    
+    <!-- Language switcher -->
+    <div>
+      <label>{{ $t('language') }}: {{ $i18n.locale }}</label>
+      <ul>
+        <li v-for="locale in locales" :key="locale.code">
+          <NuxtLink :href="switchLocalePath(locale.code)">{{ locale.name }}{{ switchLocalePath(locale.code) }}</NuxtLink>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -2,9 +2,35 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/strapi'],
+  modules: [
+    '@nuxtjs/strapi',
+    '@nuxtjs/i18n',
+  ],
 
   strapi: {
     devtools: true,
+  },
+
+  i18n: {
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json' },
+      { code: 'ru', name: 'Русский', file: 'ru.json' },
+      // { code: 'es', name: 'Español', file: 'es.json' },
+      // { code: 'fr', name: 'Français', file: 'fr.json' },
+      // { code: 'de', name: 'Deutsch', file: 'de.json' },
+      // { code: 'zh', name: '中文', file: 'zh.json' },
+      // { code: 'ja', name: '日本語', file: 'ja.json' },
+      // { code: 'ar', name: 'العربية', file: 'ar.json' },
+      // { code: 'pt', name: 'Português', file: 'pt.json' },
+      // { code: 'it', name: 'Italiano', file: 'it.json' }
+    ],
+    defaultLocale: 'ru',
+    langDir: 'locales/',
+    strategy: 'prefix_except_default',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root'
+    }
   },
 })
